988974993fbe969b8e8e94d702570ba761c8fb3e
kazeem <alevel7@gmail.com>
2022-08-07 13:01:55 +0000

    adding inital setup

---
+++ app.py
@@ -0,0 +1,15 @@
+# import click
+
+# @click.command()
+# def hello():
+#     """Prints a name out with apple at the end"""
+#   click.echo('Hello World')
+   
+   
+# if __name__ == '__main__':
+#     hello()
+
+def myFunc():
+    return 1
+    
+myFunc()
